19491572a9ba1088d94bbe10ce52bfca0cc5ed68
FazleRabbbiferdaus172 <fazle.ferdaus1416@gmail.com>
2025-09-03 02:00:44 -0500

    include urls for view and media file for serving pdf

--- config/settings.py
+++ config/settings.py
@@ -122,3 +122,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+X_FRAME_OPTIONS= 'same-origin'
+
+MEDIA_URL = '/pdf/'
+
+
+MEDIA_ROOT = BASE_DIR / 'pdf'
