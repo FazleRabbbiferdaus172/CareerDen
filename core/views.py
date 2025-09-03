9de7a669797f5d9004353bf920bc69a50880f757
FazleRabbbiferdaus172 <fazle.ferdaus1416@gmail.com>
2025-09-03 02:00:14 -0500

    views for index and pdf file

--- core/views.py
+++ core/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from latex_template_compiler import render_latex_pdf
 
 # Create your views here.
+
+def index(request):
+    return render(request, "core/index.html")
+
+def clicked(request):
+    latex = render_latex_pdf()
+    return render(request, "core/clicked.html", {"pdf_path": latex})
