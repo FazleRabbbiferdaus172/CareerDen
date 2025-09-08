18c4bcb334bc1e176fc880a6010d3808777d33e3
FazleRabbbiferdaus172 <fazle.ferdaus1416@gmail.com>
2025-09-08 02:27:54 -0500

    view and urls for the inserting context from record list and render template with record list to work

--- core/urls.py
+++ core/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("clicked/", views.clicked, name="clicked"),
     path("requied-context-info/", views.get_required_context_set_to_render_template, name="clicked"),
+    path("render_template/", views.render_template, name="render_template"),
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from latex_template_compiler import render_latex_pdf, get_required_context
+
+from latex_template_compiler import render_latex_pdf
+from .utils.latex_service import get_required_context
 
 from .models import Profile
 from config.settings import addable_model
@@ -13,7 +15,8 @@ def index(request):
              'model_name': model._meta.model_name
              }
         models.append(m)
-    context = {'models': models}
+    required_context_set = get_required_context()
+    context = {'models': models, 'required_template_context': required_context_set}
     return render(request, "core/index.html", context)
 
 def clicked(request):
@@ -22,6 +25,15 @@ def clicked(request):
     latex = render_latex_pdf(**{'name': name})
     return render(request, "core/clicked.html", {"pdf_path": latex})
 
+
+def render_template(request):
+    context = {}
+    for key in request.POST.keys():
+        if key != 'csrfmiddlewaretoken':
+            context[key] = request.POST[key]
+    latex = render_latex_pdf(context)
+    return render(request, "core/clicked.html", {"pdf_path": latex})
+
 def get_required_context_set_to_render_template(request):
     required_context_set = get_required_context()
-    return render(request, "core/required_context_set.html", {"required_context_set": required_context_set})
+    return render(request, "core/required_context_set.html", {"required_template_context": required_context_set})
