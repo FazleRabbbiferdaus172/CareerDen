4dec86456d7ec23ca272087fbe73c7326078c7fe
FazleRabbbiferdaus172 <fazle.ferdaus1416@gmail.com>
2025-09-08 02:25:17 -0500

    initial js functions for adding values to context feature to work

--- core/static/core/drag_n_drop.js
+++ core/static/core/drag_n_drop.js
@@ -1,3 +1,4 @@
+let dataToInsert = null;
 function dragstartHandler(ev) {
   ev.dataTransfer.setData("text", ev.target.id);
 }
@@ -10,4 +11,28 @@ function dropHandler(ev) {
   ev.preventDefault();
   const data = ev.dataTransfer.getData("text");
   ev.target.appendChild(document.getElementById(data));
+}
+
+function addTo(ev) {
+    dataToInsert = ev.target.previousSibling.innerText;
+    let context_children = document.getElementById("context-list").children;
+    const button_to_add = document.createElement("button");
+    button_to_add.textContent = "Insert";
+    button_to_add.addEventListener("click", insertInto);
+    button_to_add.className = "context-insert";
+    // button_to_add.addEventListener()
+    for (const child of context_children) {
+        // debugger;
+        child.appendChild(button_to_add);
+    }
+}
+
+
+function insertInto(ev) {
+    ev.target.previousElementSibling.value = dataToInsert;
+    dataToInsert = null;
+    let insert_buttons = document.getElementsByClassName("context-insert");
+    for (let insert_button of insert_buttons) {
+        insert_button.remove();
+    }
 }
