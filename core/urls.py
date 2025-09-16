bc0a629f95692cd0126b35d6164a392dab0e77d7
FazleRabbbiferdaus172 <fazle.ferdaus1416@gmail.com>
2025-09-16 01:55:05 -0500

    urls for main section and now graph urls are added

--- core/urls.py
+++ core/urls.py
@@ -5,6 +5,9 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("clicked/", views.clicked, name="clicked"),
+    path("latex_main/", views.latex_main, name="latex_main"),
     path("requied-context-info/", views.get_required_context_set_to_render_template, name="clicked"),
     path("render_template/", views.render_template, name="render_template"),
+    path("graph_data/", views.graph_data, name="graph_data"),
+    path("graph/", views.graph_view, name="graph_view"),
 ]
